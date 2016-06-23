bd3a0894fb2cc595ffb9cf8a65fb0fb99c6b3206
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-06-23 15:24:29 -0700

    added signup functionality, fixed error in test

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 'use strict';
+
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define('user', {
     name: {
       type: DataTypes.STRING,
       validate: {
         len: {
-        args: [1, 254],
-        msg: 'Name must be between 1 and 254 characters'
+          args: [1, 254],
+          msg: 'Name must be between 1 and 254 characters'
         }
       }
     },
@@ -27,10 +30,29 @@ module.exports = function(sequelize, DataTypes) {
         }
       }
     }
-  },{
+  }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      },
+      toJSON: function() {
+        var jsonUser = this.get();
+        delete jsonUser.password;
+
+        return jsonUser;
+      }
+    },
+    hooks: {
+      beforeCreate: function(createdUser, options, cb) {
+        // hash password and save hash to user
+        var hash = bcrypt.hashSync(createdUser.password, 10);
+        createdUser.password = hash;
+        cb(null, createdUser);
       }
     }
   });
